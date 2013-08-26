15c3bf9158b7be6ddbaab07743829f6d6b26a266
Simon <simon.taranto@gmail.com>
2013-08-26 08:32:25 -0600

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PersonalTracker::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  # NEED TO DEFINE ROOT FOR DEVISE
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
